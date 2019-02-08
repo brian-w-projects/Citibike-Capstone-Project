813e72f0e4ba1f6671ba8469572092b3e7c52d3a
brian.weinfeld+cuny@gmail.com <brian.weinfeld+cuny@gmail.com>
2019-02-07 16:41:10 -0800

    weather scraping

--- scrape/scrape_old_data.py
+++ scrape/scrape_old_data.py
@@ -6,6 +6,9 @@ import zipfile
 import requests
 import json
 import csv
+from datetime import datetime, timedelta
+from dateutil import tz
+import re
 
 
 def scrape_ride_data():
@@ -59,3 +62,56 @@ def scrape_station_data():
             writer.writerow([s['station_id'], s['lat'], s['lon'], s['name']])
     os.chdir('..')
     print('Finished')
+
+
+def scrape_weather(time=datetime(2013, 1, 1, 0, 0, 0, tzinfo=tz.gettz('America/New_York')), count=900):
+    """
+    Scrapes Weather From API
+    """
+
+    path = os.path.join(os.getcwd(), 'weather')
+    key = ''
+    lat = '40.730610'
+    lon = '-73.935242'
+
+    if not os.path.exists(path):
+        os.mkdir(path)
+    os.chdir(path)
+
+    with open('weather.csv', 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',')
+        for day_count in range(count):
+            print(time.strftime("%Y-%m-%dT%H:%M:%S%z"))
+            url = 'https://api.darksky.net/forecast/{}/{},{},{}?exclude=currently,flags'.format(
+                key, lat, lon, time.strftime("%Y-%m-%dT%H:%M:%S%z"))
+            r = requests.get(url, 'json')
+            for hour in scrape_day(json.loads(r.content)):
+                writer.writerow(hour)
+            time += timedelta(days=1)
+    os.chdir('..')
+
+def scrape_day(day):
+    from_zone = tz.gettz('UTC')
+    to_zone = tz.gettz('America/New_York')
+    sunrise = day['daily']['data'][0]['sunriseTime']
+    sunset = day['daily']['data'][0]['sunsetTime']
+
+    for ele in day['hourly']['data']:
+        hourly = list()
+        hourly.append(
+            datetime.utcfromtimestamp(ele['time']).replace(tzinfo=from_zone).astimezone(to_zone).strftime(
+                '%Y-%m-%d %H:%M:%S')
+        )
+        if sunrise <= ele['time'] <= sunset:
+            hourly.append(1)
+        else:
+            hourly.append(0)
+        hourly.append(re.sub('(-night)|(-day)', '', ele['icon'].lower()))
+        hourly.append(ele['precipProbability'])
+        hourly.append(ele['apparentTemperature'])
+        hourly.append(ele['humidity'])
+        hourly.append(ele['windSpeed'])
+        yield hourly
+
+
+# scrape_weather(time=datetime(2013, 1, 11, 0, 0, 0, tzinfo=tz.gettz('America/New_York')), count=900)
